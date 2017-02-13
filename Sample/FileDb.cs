6dd1952d12cd8a4823968622a3727a1a7614c965
almanzharoz <magicb1985@gmail.com>
2017-02-13 17:23:23 +0500

    Add files via upload

---
+++ Sample/FileDb.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using SharpFuncExt;
+
+namespace SharpFunc.ConsoleApp
+{
+    public class FileDb : IDisposable
+    {
+	    private readonly FileStream _file;
+	    public FileDb(string filename)
+	    {
+		    _file = new FileStream(filename, FileMode.OpenOrCreate);
+	    }
+
+	    public void Dispose() => _file.Dispose();
+
+	    public byte[] ReadBytes(long pos, int length) =>
+		    pos.Extend(length)
+			    .ThrowIf(x => x.Item1 < 0 || x.Item2 <= 0 || x.Item1 + x.Item2 > _file.Length, x => new ArgumentOutOfRangeException($"Pos: {x.Item1}, Length: {x.Item2}, FileLength: {_file.Length}"))
+			    .Fluent(x => _file.Seek(x.Item1, SeekOrigin.Begin))
+			    .Convert(x => new byte[x.Item2].Fluent(y => _file.Read(y, 0, y.Length)));
+
+	    public void WriteBytes(long pos, byte[] buffer) =>
+		    _file.Write(buffer.HasNotNullArg(nameof(buffer))
+			    .Fluent(x => _file.Seek(pos.ThrowIf(y => y < 0, e => new ArgumentOutOfRangeException()),
+					    SeekOrigin.Begin)), 0, buffer.Length);
+    }
+}
---
+++ Sample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpFuncExt;
+
+namespace SharpFunc.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+	        args.HasNotNullArg(nameof(args))
+		        .Using(x => new FileDb(x[0]), (a, f) => a
+			        .Switch(x => x[1].ToLower() == "read", x => Read(f, x[2].ToInt64(), x[3].ToInt32()))
+			        .Switch(x => x[1].ToLower() == "write", x => Write(f, x[2], x[3].ToInt64())))
+		        .Fluent(x => Console.ReadLine());
+        }
+
+	    private static void Write(FileDb file, string s, long pos) => file.WriteBytes(pos, Encoding.UTF8.GetBytes(s));
+
+	    private static void Read(FileDb file, long pos, int length)
+		    => Console.WriteLine(Encoding.UTF8.GetString(file.ReadBytes(pos, length)));
+
+    }
+}
